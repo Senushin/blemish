d25c5903203f9c058850a208942a4941f8d64179
Senushin <54420920+Senushin@users.noreply.github.com>
2019-09-25 20:08:51 +0300

    Update blemish.py

--- blemish.py
+++ blemish.py
@@ -2,33 +2,40 @@ import cv2
 import numpy as np
 
 img = cv2.imread('image.png')
+mask = np.ones((30,30,3),np.uint8)+245
+r = 15 # radius
 
 def func1(action, x, y, flags, userdata):
-    global top, bottom
+    #global mask
     if action==cv2.EVENT_LBUTTONDOWN:
-        cv2.destroyWindow('Blemish zone')
-        top=[(x,y)]
-        imgCopy = img.copy()
-        #cv2.putText(imgCopy,'center is: ' + str(center),
-            #(10,450), cv2.FONT_HERSHEY_SIMPLEX,
-            #0.7,(255,255,255), 2 );
-        cv2.imshow('Blemish',imgCopy)
-    elif action==cv2.EVENT_LBUTTONUP:
-        bottom=[(x,y)]
-        zone = img [top[0][1]:bottom[0][1],top[0][0]:bottom[0][0]]
-        #zone = zone.astype(cv2.CV_32F)
-        #dft = cv2.dft(zone,flags = cv2.DFT_COMPLEX_OUTPUT)
-        grayzone = cv2.cvtColor(zone, cv2.COLOR_BGR2GRAY)
-        f = cv2.dft(np.float32(grayzone), flags=cv2.DFT_COMPLEX_OUTPUT)
-        f_shift = np.fft.fftshift(f)
-        f_complex = f_shift[:,:,0] + 1j*f_shift[:,:,1]
-        f_abs = np.abs(f_complex) + 1 # lie between 1 and 1e6
-        f_bounded = 20 * np.log(f_abs)
-        f_img = 255 * f_bounded / np.max(f_bounded)
-        f_img = f_img.astype(np.uint8)
-        print (type(f), f.shape)
-        cv2.imshow('Blemish zone',f_img)
-        zone = 0
+        magList = []
+
+        for x_modl in [-(r*2), 0, r*2]:
+            for y_modl in [-(r*2), 0, r*2]:
+                x_mod = x+x_modl
+                y_mod = y+y_modl
+                center=[x_mod,y_mod]
+                zone = img [y_mod-r:y_mod+r,x_mod-r:x_mod+r]
+                grayzone = cv2.cvtColor(zone, cv2.COLOR_BGR2GRAY)
+                f = np.fft.fft2(grayzone)
+                fshift = np.fft.fftshift(f)
+                magnitude_spectrum = 20*np.log(np.abs(fshift))
+                f_ishift = np.fft.ifftshift(fshift)
+                mag = [np.sum(magnitude_spectrum),center]
+                #print(mag)
+                magList.append(mag)
+                zone = 0
+
+        blemish = magList[4]
+        replaser = min(magList)
+        x_b, y_b = blemish [1][0], blemish [1][1]
+        x_r, y_r = replaser [1][0], replaser [1][1]
+        zone_b = img [y_b-r:y_b+r,x_b-r:x_b+r]
+        zone_r = img [y_r-r:y_r+r,x_r-r:x_r+r]
+        zone_sc = cv2.seamlessClone(zone_r, zone_b,mask, (r,r), cv2.NORMAL_CLONE)
+        img[y-r:y+r,x-r:x+r] = zone_sc
+        cv2.imshow('Blemish',img)
+
 cv2.namedWindow('Blemish')
 cv2.setMouseCallback('Blemish', func1)
 while True:
@@ -37,3 +44,4 @@ while True:
     if f ==27:
         break
 cv2.destroyAllWindows()
+
